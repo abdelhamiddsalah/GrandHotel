c2a262f4b840ed478dabe3fe4e603cf79070458e
Abdelhamid Salah <abdelhamidsalah222@gmail.com>
2026-01-02 13:32:06 +0200

    add colors in styles

---
+++ lib/core/styles/colors.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+class Colors {
+  static const Color primary = Color(0xFF2E3192);
+  static const Color accent = Color(0xFF00BCD4);
+  static const Color background = Color(0xFFFFFFFF);
+  static const Color textPrimary = Color(0xFF000000);
+  static const Color textSecondary = Color(0xFF757575);
+}
